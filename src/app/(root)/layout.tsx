1004f38c063016efb9c81b0c3a9469fb9e13daed
kwakjaehun <jh5414092@icloud.com>
2025-07-30 02:35:31 +0900

    Feat: vercel analytics 설치

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,10 +1,12 @@
 import ClientProviders from "@/components/providers/ClientProviders";
+import { Analytics } from "@vercel/analytics/next";
 import { PropsWithChildren } from "react";
 
 const RootLayout = ({ children }: PropsWithChildren) => {
   return (
     <>
       <ClientProviders>{children}</ClientProviders>
+      <Analytics />
     </>
   );
 };
