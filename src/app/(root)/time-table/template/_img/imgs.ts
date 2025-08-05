ccfd4f4b05f244af44fbcb0236caf0d69b0a1634
kwakjaehun <jh5414092@icloud.com>
2025-08-05 21:30:33 +0900

    Feat: template

---
+++ src/app/(root)/time-table/template/_img/imgs.ts
@@ -0,0 +1,34 @@
+import { ImgsType } from "@/types/time-table/image";
+import MainBG from "./main/bg_dohwabi.png";
+import MainOffline from "./main/offline_dohwabi.png";
+import MainOnline from "./main/online_dohwabi.png";
+import MainPlaceholderImage from "./main/placeholder.png";
+import MainProfile from "./main/profile_dohwabi.png";
+import MainWeek from "./main/week_dohwabi.png";
+
+export const Imgs: ImgsType = {
+  first: {
+    bg: MainBG,
+    offline: MainOffline,
+    online: MainOnline,
+    week: MainWeek,
+    profile: MainProfile,
+    placeholder: MainPlaceholderImage,
+  },
+  second: {
+    bg: MainBG,
+    offline: MainOffline,
+    online: MainOnline,
+    week: MainWeek,
+    profile: MainProfile,
+    placeholder: MainPlaceholderImage,
+  },
+  third: {
+    bg: MainBG,
+    offline: MainOffline,
+    online: MainOnline,
+    week: MainWeek,
+    profile: MainProfile,
+    placeholder: MainPlaceholderImage,
+  },
+};
---
+++ src/app/(root)/time-table/template/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import TimeTableEditor from "./_components/TimeTableEditor";
+import "./_styles/index.css";
+
+const TestPage = () => {
+  return (
+    <div className={`fixed inset-0 w-full h-full`}>
+      <TimeTableEditor />
+    </div>
+  );
+};
+
+export default TestPage;
