a96095e38123b74eb5085faf75ca8577b3af642d
kwakjaehun <jh5414092@icloud.com>
2025-08-24 19:16:25 +0900

    Feat: 포폴 갤러리 로직 수정

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,13 +1,18 @@
 export const splitOddEven = (n: number) => {
   const odd = [];
   const even = [];
+  const start = 11;
 
-  for (let i = 1; i <= n; i++) {
+  for (let i = start; i <= n; i++) {
     if (i % 2 === 1) {
       odd.push(i);
     } else {
       even.push(i);
     }
+  }
+
+  if (n % 2 === 1) {
+    even.push(start);
   }
 
   return [odd, even];
