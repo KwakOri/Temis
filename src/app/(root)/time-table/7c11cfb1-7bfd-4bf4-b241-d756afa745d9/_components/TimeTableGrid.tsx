f500fa6b6daceaef2f1216c452ced804eacab978
kwakjaehun <jh5414092@icloud.com>
2025-09-21 00:32:27 +0900

    Feat: 템플릿 완성

---
+++ src/app/(root)/time-table/7c11cfb1-7bfd-4bf4-b241-d756afa745d9/_components/TimeTableGrid.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+import { TTheme } from "@/types/time-table/theme";
+import { TDefaultCard } from "@/utils/time-table/data";
+import { Imgs } from "../_img/imgs";
+import TimeTableCell from "./TimeTableCell";
+
+interface TimeTableGridProps {
+  data: TDefaultCard[];
+  weekDates: Date[];
+  currentTheme: TTheme;
+}
+
+const TimeTableGrid: React.FC<TimeTableGridProps> = ({
+  data,
+  weekDates,
+  currentTheme,
+}) => {
+  return (
+    <>
+      <div
+        style={{
+          width: 4000,
+          height: 2250,
+          position: "absolute",
+          zIndex: 30,
+        }}
+      >
+        <img
+          src={Imgs["first"]["sticker"].src}
+          alt={"top-object"}
+          draggable={false}
+        />
+      </div>
+      <div
+        className="absolute grid grid-cols-4 z-20"
+        style={{
+          top: 600,
+          left: 20,
+          columnGap: 20,
+          rowGap: 68,
+          rotate: "-2.5deg",
+        }}
+      >
+        {data.map((time, i) => (
+          <TimeTableCell
+            key={time.day}
+            time={time}
+            currentTheme={currentTheme}
+            weekDate={weekDates[i]}
+            index={i}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default TimeTableGrid;
