f072146b3be573c5c6dd7426ab24ca7d882ad154
kwakjaehun <jh5414092@icloud.com>
2025-09-27 21:31:07 +0900

    Feat: 도희님 템플릿 완성

---
+++ src/app/(root)/time-table/dd1611a2-9773-447d-b711-e04ea77e2490/_img/imgs.ts
@@ -0,0 +1,24 @@
+import { ImgsType } from "@/types/time-table/image";
+
+// Background and main images
+import ArtistImg from "./main/artist_dohee.png";
+import MainBG from "./main/bg_dohee.png";
+import OnlineImg from "./main/online_dohee.png";
+import TopObject from "./main/topobject_dohee.png";
+
+// Offline images
+import OfflineImg from "./main/offline_dohee.png";
+
+// Profile images
+import MainProfileFrame from "./main/profile_dohee.png";
+
+export const Imgs: ImgsType = {
+  first: {
+    bg: MainBG,
+    online: OnlineImg,
+    artist: ArtistImg,
+    topObject: TopObject,
+    offline: OfflineImg,
+    profileFrame: MainProfileFrame,
+  },
+};
