37bb383f895a857484b94d711bfa8308760b4d99
kwakjaehun <jh5414092@icloud.com>
2025-10-08 10:15:01 +0900

    Feat: 테미스 템플릿 vol.2 색상별로 업로드

---
+++ src/app/(root)/time-table/bee27a39-ef1c-4cc5-b28d-f95643e5e942/_img/imgs.ts
@@ -0,0 +1,28 @@
+import { ImgsType } from "@/types/time-table/image";
+
+// Background and main images
+import Artist from "./main/artist_Y2Kyellow.png";
+import MainBG from "./main/bg_Y2Kyellow.png";
+
+// Online/Offline images
+import MemoImg from "./main/memo_Y2Kyellow.png";
+import OfflineImg from "./main/offline_Y2Kyellow.png";
+import OnlineImg from "./main/online1_Y2Kyellow.png";
+import Online2Img from "./main/online2_Y2Kyellow.png";
+
+// Profile images
+import MainProfileFrame from "./main/profile_Y2Kyellow.png";
+import TopObject from "./main/topobject_Y2Kyellow.png";
+
+export const Imgs: ImgsType = {
+  first: {
+    bg: MainBG,
+    artist: Artist,
+    offline: OfflineImg,
+    online: OnlineImg,
+    bigOnline: Online2Img,
+    memo: MemoImg,
+    profileFrame: MainProfileFrame,
+    topObject: TopObject,
+  },
+};
