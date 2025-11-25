aff067c690d8e6632fc1791a7978515f66978a40
kwakjaehun <jh5414092@icloud.com>
2025-11-25 12:31:41 +0900

    Feat: 리네님 시간표 완성

---
+++ src/app/(root)/time-table/47d9b0fa-ad98-46ae-b849-374e9ae09445/_components/_uneditable/TimeTableContent.tsx
@@ -0,0 +1,65 @@
+import { useTimeTableData, useTimeTableUI } from "@/contexts/TimeTableContext";
+import React from "react";
+
+import TimeTableDesignGuide from "@/components/tools/TimeTableDesignGuide";
+import { TDefaultCard, TPlaceholders } from "@/types/time-table/data";
+import { TTheme } from "@/types/time-table/theme";
+import { isGuideEnabled } from "@/utils/time-table/data";
+import { Imgs } from "../../_img/imgs";
+import { templateSize } from "../../_settings/settings";
+import ProfileImageSection from "../ProfileImageContainer";
+import TimeTableGrid from "../TimeTableGrid";
+import TimeTableTopObject from "../TimeTableTopObject";
+import TimeTableWeekFlag from "../TimeTableWeekFlag";
+
+export interface TimeTableContentProps {
+  currentTheme: TTheme;
+  data: TDefaultCard[];
+  placeholders: TPlaceholders;
+}
+
+const TimeTableContent: React.FC<TimeTableContentProps> = ({
+  currentTheme,
+  data,
+  placeholders,
+}) => {
+  const { imageSrc, weekDates, profileText } = useTimeTableData();
+  const { scale, isProfileTextVisible } = useTimeTableUI();
+
+  if (weekDates.length === 0) return null;
+
+  return (
+    <div
+      id="timetable"
+      className=" box-border select-none font-sans origin-top-left relative overflow-hidden shadow-[0_6px_20px_rgba(0,0,0,0.15)]"
+      style={{
+        transform: `scale(${scale})`,
+        backgroundImage: `url(${Imgs[currentTheme].bg.src})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        width: templateSize.width,
+        height: templateSize.height,
+      }}
+    >
+      {isGuideEnabled && <TimeTableDesignGuide />}
+      <TimeTableTopObject />
+
+      <TimeTableWeekFlag currentTheme={currentTheme} weekDates={weekDates} />
+      <TimeTableGrid
+        data={data}
+        weekDates={weekDates}
+        currentTheme={currentTheme}
+      />
+      <ProfileImageSection
+        imageSrc={imageSrc}
+        profileText={profileText}
+        profileTextPlaceholder={placeholders.profileText}
+        currentTheme={currentTheme}
+        isProfileTextVisible={isProfileTextVisible}
+      />
+    </div>
+  );
+};
+
+export default TimeTableContent;
