2ffc49bab799446fac8819836491ef43ad3056e9
kwakjaehun <jh5414092@icloud.com>
2025-12-02 22:14:08 +0900

    Feat: 라봉님 시간표 완성

---
+++ src/app/(root)/time-table/5bbe51f4-9ab1-4f52-b23b-29e2de9b8eef/_components/ProfileImageContainer.tsx
@@ -0,0 +1,180 @@
+import { AutoResizeText } from "@/components/AutoResizeTextCard";
+import { TTheme } from "@/types/time-table/theme";
+import { PropsWithChildren } from "react";
+import { Imgs } from "../_img/imgs";
+import {
+  colors,
+  fontOption,
+  profileFrameHeight,
+  profileFrameWidth,
+  profileImageHeight,
+  profileImageInfo,
+  profileImageWidth,
+} from "../_settings/settings";
+
+interface ProfileBackPlateProps {
+  currentTheme?: TTheme;
+}
+
+interface ProfileImageProps {
+  imageSrc: string | null;
+}
+
+interface ProfileTextProps {
+  profileText: string;
+  profileTextPlaceholder: string;
+  isProfileTextVisible: boolean;
+}
+
+interface ProfileImageSectionProps {
+  currentTheme: TTheme;
+  imageSrc: string | null;
+  profileText: string;
+  profileTextPlaceholder: string;
+  isProfileTextVisible: boolean;
+}
+
+const ProfileBackPlate = ({ currentTheme }: ProfileBackPlateProps) => {
+  return (
+    <div
+      style={{
+        zIndex: "0",
+      }}
+      className="absolute inset-0"
+    >
+      <img
+        src={Imgs[currentTheme || "first"]["profileBG"].src.replace("./", "/")}
+        alt="profileBG"
+        className="object-cover w-full h-full"
+        draggable={false}
+      />
+    </div>
+  );
+};
+
+const ProfileImage = ({ imageSrc }: ProfileImageProps) => {
+  return (
+    <div
+      style={{
+        width: profileImageWidth,
+        height: profileImageHeight,
+        position: "absolute",
+        top: 476,
+        right: 36,
+        rotate: "3.6deg",
+        zIndex: profileImageInfo.arrange === "onTop" ? 20 : 10,
+      }}
+    >
+      {imageSrc && (
+        <img
+          className="object-cover w-full h-full"
+          src={imageSrc}
+          alt={"placeholder"}
+        />
+      )}
+    </div>
+  );
+};
+
+const ProfileFrame = () => {
+  return (
+    <div
+      style={{
+        width: profileFrameWidth,
+        height: profileFrameHeight,
+        zIndex: profileImageInfo.arrange === "onTop" ? 10 : 20,
+        position: "absolute",
+      }}
+    >
+      <img
+        src={Imgs["first"]["profile"].src.replace("./", "/")}
+        alt="frame"
+        className="object-cover"
+        draggable={false}
+      />
+    </div>
+  );
+};
+
+const ProfileTextTitle = () => {
+  return <p style={{ fontSize: 38, width: 172 }}>ART BY ::</p>;
+};
+
+const ProfileText = ({
+  profileText,
+  profileTextPlaceholder,
+  isProfileTextVisible,
+}: ProfileTextProps) => {
+  if (!isProfileTextVisible) return null;
+  return (
+    <div
+      style={{
+        width: 4000,
+        height: 2250,
+      }}
+      className="absolute z-50 flex flex-col items-start "
+    >
+      <div
+        style={{
+          width: 640,
+          height: 120,
+          zIndex: 20,
+          bottom: 80,
+          right: 610,
+          rotate: "-3.1deg",
+        }}
+        className="absolute flex justify-center items-center "
+      >
+        <AutoResizeText
+          style={{
+            lineHeight: 0,
+            color: colors["first"]["primary"],
+            fontFamily: fontOption.secondary,
+          }}
+          className="text-center"
+          maxFontSize={100}
+        >
+          {profileText ? profileText : profileTextPlaceholder}
+        </AutoResizeText>
+      </div>
+      <img src={Imgs["first"]["artist"].src} alt="artist" />
+    </div>
+  );
+};
+
+const ProfileImageContainer = ({ children }: PropsWithChildren) => {
+  return (
+    <div
+      className={`absolute flex justify-center z-10`}
+      style={{
+        width: 4000,
+        height: 2250,
+      }}
+      draggable={false}
+    >
+      {children}
+    </div>
+  );
+};
+
+const ProfileImageSection = ({
+  currentTheme,
+  imageSrc,
+  profileText,
+  profileTextPlaceholder,
+  isProfileTextVisible,
+}: ProfileImageSectionProps) => {
+  return (
+    <ProfileImageContainer>
+      <ProfileText
+        isProfileTextVisible={isProfileTextVisible}
+        profileText={profileText}
+        profileTextPlaceholder={profileTextPlaceholder}
+      />
+      <ProfileFrame />
+      <ProfileImage imageSrc={imageSrc} />
+    </ProfileImageContainer>
+  );
+};
+
+export default ProfileImageSection;
---
+++ src/app/(root)/time-table/5bbe51f4-9ab1-4f52-b23b-29e2de9b8eef/_img/imgs.ts
@@ -0,0 +1,24 @@
+import { ImgsType } from "@/types/time-table/image";
+
+// Background and main images
+import artist_rabong from "./main/artist_rabong.png";
+import bg_rabong from "./main/bg_rabong.png";
+import offline_brown_rabong from "./main/offline_brown_rabong.png";
+import offline_orange_rabong from "./main/offline_orange_rabong.png";
+import online_brown_rabong from "./main/online_brown_rabong.png";
+import online_orange_rabong from "./main/online_orange_rabong.png";
+import profile_rabong from "./main/profile_rabong.png";
+import top_object_rabong from "./main/top_object_rabong.png";
+
+export const Imgs: ImgsType = {
+  first: {
+    bg: bg_rabong,
+    artist: artist_rabong,
+    profile: profile_rabong,
+    offline_brown: offline_brown_rabong,
+    online_brown: online_brown_rabong,
+    offline_orange: offline_orange_rabong,
+    online_orange: online_orange_rabong,
+    topObject: top_object_rabong,
+  },
+};
