b12362b4ad57cea978f77921657929e900cce93f
kwakjaehun <jh5414092@icloud.com>
2025-12-13 23:00:21 +0900

    Feat: 미치르님 시간표 완성

---
+++ src/app/(root)/time-table/75e91923-7fde-4ee8-84f5-daed62b838d0/_components/TimeTableGrid.tsx
@@ -0,0 +1,45 @@
+import React, { Fragment } from "react";
+
+import { TDefaultCard } from "@/types/time-table/data";
+import { TTheme } from "@/types/time-table/theme";
+import TimeTableCell from "./TimeTableCell";
+
+interface TimeTableGridProps {
+  data: TDefaultCard[];
+  weekDates: Date[];
+  currentTheme: TTheme;
+}
+
+const TimeTableGrid: React.FC<TimeTableGridProps> = ({
+  data,
+  weekDates,
+  currentTheme,
+}) => {
+  return (
+    <div
+      className="absolute grid grid-cols-3 justify-center items-center z-20"
+      style={{
+        top: 214,
+        left: 1664,
+        rowGap: 8,
+        columnGap: 72,
+        rotate: "-3.8deg",
+      }}
+    >
+      <div></div>
+      <div></div>
+      {data.map((time, i) => (
+        <Fragment key={time.day}>
+          <TimeTableCell
+            time={time}
+            currentTheme={currentTheme}
+            weekDate={weekDates[i]}
+            index={i}
+          />
+        </Fragment>
+      ))}
+    </div>
+  );
+};
+
+export default TimeTableGrid;
