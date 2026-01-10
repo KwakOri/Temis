a4ec4b7530210887cd86b65dafcd6e6650a7d0ee
kwakjaehun <jh5414092@icloud.com>
2026-01-10 23:51:05 +0900

    Feat: 에궁님 시간표 완성

---
+++ src/app/(root)/time-table/ddc90608-8f70-48a6-87da-78fadb76aece/_components/ProfileImageContainer.tsx
@@ -0,0 +1,185 @@
+import AutoResizeText from "@/components/AutoResizeTextCard/AutoResizeText";
+import { TTheme } from "@/types/time-table/theme";
+import { PropsWithChildren } from "react";
+import { Imgs } from "../_img/imgs";
+import {
+  colors,
+  fontOption,
+  profileFrameHeight,
+  profileFrameWidth,
+  profileImageHeight,
+  profileImageInfo,
+  profileImageWidth,
+} from "../_settings/settings";
+
+interface ProfileBackPlateProps {
+  currentTheme?: TTheme;
+}
+
+interface ProfileImageProps {
+  imageSrc: string | null;
+}
+
+interface ProfileTextProps {
+  profileText: string;
+  profileTextPlaceholder: string;
+  isProfileTextVisible: boolean;
+}
+
+interface ProfileImageSectionProps {
+  currentTheme: TTheme;
+  imageSrc: string | null;
+  profileText: string;
+  profileTextPlaceholder: string;
+  isProfileTextVisible: boolean;
+}
+
+const ProfileBackPlate = ({ currentTheme }: ProfileBackPlateProps) => {
+  return (
+    <div
+      style={{
+        zIndex: "0",
+      }}
+      className="absolute inset-0"
+    >
+      <img
+        src={Imgs[currentTheme || "first"]["profileBG"].src.replace("./", "/")}
+        alt="profileBG"
+        className="object-cover w-full h-full"
+        draggable={false}
+      />
+    </div>
+  );
+};
+
+const ProfileImage = ({ imageSrc }: ProfileImageProps) => {
+  return (
+    <div
+      style={{
+        width: profileImageWidth,
+        height: profileImageHeight,
+        position: "absolute",
+        bottom: -28,
+        left: 2048,
+        rotate: "2.3deg",
+        zIndex: profileImageInfo.arrange === "onTop" ? 20 : 50,
+      }}
+    >
+      {imageSrc && (
+        <img
+          className="object-cover w-full h-full"
+          src={imageSrc}
+          alt={"placeholder"}
+        />
+      )}
+    </div>
+  );
+};
+
+const ProfileFrame = () => {
+  return (
+    <div
+      style={{
+        width: profileFrameWidth,
+        height: profileFrameHeight,
+        zIndex: profileImageInfo.arrange === "onTop" ? 10 : 20,
+        position: "absolute",
+      }}
+    >
+      <img
+        src={Imgs["first"]["profileFrame"].src.replace("./", "/")}
+        alt="frame"
+        className="object-cover"
+        draggable={false}
+      />
+    </div>
+  );
+};
+
+const ProfileTextTitle = () => {
+  return <p style={{ fontSize: 38, width: 172 }}>ART BY ::</p>;
+};
+
+const ProfileText = ({
+  profileText,
+  profileTextPlaceholder,
+  isProfileTextVisible,
+}: ProfileTextProps) => {
+  if (!isProfileTextVisible) return null;
+  return (
+    <div
+      style={{
+        width: 4000,
+        height: 2250,
+      }}
+      className="absolute z-50 flex justify-end items-center "
+    >
+      <div
+        style={{
+          position: "absolute",
+          height: 160,
+          width: 780,
+          zIndex: 20,
+          left: 2382,
+          bottom: -8,
+          rotate: "2.3deg",
+        }}
+        className="flex justify-center items-center "
+      >
+        <AutoResizeText
+          style={{
+            lineHeight: 1,
+            color: colors["first"]["primary"],
+            fontFamily: fontOption.primary,
+          }}
+          className="text-center"
+          maxFontSize={96}
+        >
+          {profileText ? profileText : profileTextPlaceholder}
+        </AutoResizeText>
+      </div>
+      <img
+        src={Imgs["first"]["artist"].src}
+        className="object-cover"
+        alt="artist"
+      />
+    </div>
+  );
+};
+
+const ProfileImageContainer = ({ children }: PropsWithChildren) => {
+  return (
+    <div
+      className={`absolute flex justify-center z-10`}
+      style={{
+        width: 4000,
+        height: 2250,
+      }}
+      draggable={false}
+    >
+      {children}
+    </div>
+  );
+};
+
+const ProfileImageSection = ({
+  currentTheme,
+  imageSrc,
+  profileText,
+  profileTextPlaceholder,
+  isProfileTextVisible,
+}: ProfileImageSectionProps) => {
+  return (
+    <ProfileImageContainer>
+      <ProfileText
+        isProfileTextVisible={isProfileTextVisible}
+        profileText={profileText}
+        profileTextPlaceholder={profileTextPlaceholder}
+      />
+      <ProfileFrame />
+      <ProfileImage imageSrc={imageSrc} />
+    </ProfileImageContainer>
+  );
+};
+
+export default ProfileImageSection;
