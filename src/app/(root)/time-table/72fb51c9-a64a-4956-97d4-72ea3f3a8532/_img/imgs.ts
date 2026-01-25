d7bdcfae6a1433bc50cecd41e6edfc997338adf8
kwakjaehun <jh5414092@icloud.com>
2026-01-25 11:56:24 +0900

    Feat: 함준님 시간표 완성

---
+++ src/app/(root)/time-table/72fb51c9-a64a-4956-97d4-72ea3f3a8532/_img/imgs.ts
@@ -0,0 +1,28 @@
+import { ImgsType } from '@/types/time-table/image';
+import artist from './main/artist.png';
+import bg from './main/bg.png';
+import frame from './main/frame.png';
+import memo from './main/memo.png';
+import memo_dummy from './main/memo_dummy.png';
+import multi_a from './main/multi_a.png';
+import multi_b from './main/multi_b.png';
+import offline from './main/offline.png';
+import online_a from './main/online_a.png';
+import online_b from './main/online_b.png';
+import top_object from './main/top_object.png';
+
+export const Imgs: ImgsType = {
+  first: {
+    artist,
+    bg,
+    frame,
+    memo_dummy,
+    memo,
+    multi_a,
+    multi_b,
+    offline,
+    online_a,
+    online_b,
+    top_object,
+  },
+};
