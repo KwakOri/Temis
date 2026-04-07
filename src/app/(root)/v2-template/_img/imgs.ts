ab4a8fb7c3557463c8d759f2c4dffbe8454f3a49
kwakjaehun <jh5414092@icloud.com>
2026-04-07 15:15:55 +0900

    chore(v2-template): update default image assets and registry

--- src/app/(root)/v2-template/_img/imgs.ts
+++ src/app/(root)/v2-template/_img/imgs.ts
@@ -1,26 +1,24 @@
 import { ImgsType } from "@/types/time-table/image";
 
-// Background and main images
-// import ArtistImg from "./main/artist.png";
+// Prefix-based mapping for files copied from Y2K assets
+import ArtistImg from "./main/artist.png";
 import MainBG from "./main/bg.png";
-import TopObject from "./main/top_object.png";
-
-// Online images
-import OnlineImg from "./main/online.png";
-
-// Offline images
+import MemoImg from "./main/memo.png";
 import OfflineImg from "./main/offline.png";
-
-// Profile images
+import OnlineImg from "./main/online.png";
+import Online2Img from "./main/online2.png";
 import MainProfileFrame from "./main/frame.png";
+import TopObject from "./main/top_object.png";
 
 export const Imgs: ImgsType = {
   first: {
     bg: MainBG,
-    // artist: ArtistImg,
+    artist: ArtistImg,
     topObject: TopObject,
     offline: OfflineImg,
     online: OnlineImg,
+    bigOnline: Online2Img,
+    memo: MemoImg,
     profileFrame: MainProfileFrame,
   },
 };
