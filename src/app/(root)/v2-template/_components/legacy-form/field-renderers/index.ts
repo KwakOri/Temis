0bb047e3d28117d6249aea739ca03a3efebf16c5
kwakjaehun <jh5414092@icloud.com>
2026-04-09 11:31:32 +0900

    chore(v2-template): remove unused legacy form module

--- src/app/(root)/v2-template/_components/legacy-form/field-renderers/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as V2AdaptiveTimeRenderer } from "./adaptive-time-renderer";
-export { default as V2DescriptionRenderer } from "./description-renderer";
-export { default as V2ScrollableTimePicker } from "./scrollable-time-picker";
-export { default as V2TopicRenderer } from "./topic-renderer";
