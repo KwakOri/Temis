7aa42896a072d26615098e4c969bf4a3b2d212ab
kwakjaehun <jh5414092@icloud.com>
2026-05-18 16:02:36 +0900

    Remove obsolete timetable template

--- src/app/(root)/time-table/db8f0082-b6c4-4b8c-9dfc-ec336bea0566/_settings/general.ts
+++
@@ -1,8 +0,0 @@
-import { TPlaceholders } from "@/types/time-table/data";
-import { getPlaceholders } from "@/utils/time-table/data";
-import { CARD_INPUT_CONFIG, profileTextPlaceholder } from "./settings";
-
-export const placeholders: TPlaceholders = getPlaceholders({
-  cardInputConfig: CARD_INPUT_CONFIG,
-  profilePlaceholder: profileTextPlaceholder,
-});
