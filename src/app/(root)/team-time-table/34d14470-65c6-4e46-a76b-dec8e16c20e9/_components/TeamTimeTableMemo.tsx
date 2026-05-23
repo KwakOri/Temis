75bad2e3e99bc8f27a2037f93ecb68733c32e67f
kwakjaehun <jh5414092@icloud.com>
2026-05-24 00:09:12 +0900

    Feat: 비정상 스튜디오 팀 스케줄러 제작

---
+++ src/app/(root)/team-time-table/34d14470-65c6-4e46-a76b-dec8e16c20e9/_components/TeamTimeTableMemo.tsx
@@ -0,0 +1,40 @@
+import { AutoResizeText } from '@/components/AutoResizeTextCard';
+import { useTimeTableData } from '@/contexts/TimeTableContext';
+import { colors, fontOption } from '../_settings/settings';
+
+const TeamTimeTableMemo = () => {
+  const { memoText, isMemoTextVisible } = useTimeTableData();
+  return (
+    <div
+      style={{
+        width: 4000,
+        height: 2250,
+        position: 'absolute',
+        zIndex: 30,
+      }}
+      className="flex justify-center items-center"
+    >
+      {isMemoTextVisible && (
+        <div
+          className="absolute flex justify-center items-center"
+          style={{ top: 1940, width: 3600, height: 200 }}
+        >
+          <AutoResizeText
+            style={{
+              fontFamily: fontOption.primary,
+              color: colors.first.primary,
+              textAlign: 'center',
+              lineHeight: 1.2,
+            }}
+            maxFontSize={92}
+            multiline
+          >
+            {memoText}
+          </AutoResizeText>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TeamTimeTableMemo;
