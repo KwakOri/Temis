da48b9194a39f99abe6131a089e3a803147c51b7
kwakjaehun <jh5414092@icloud.com>
2026-06-07 03:13:26 +0900

    Feat: 듀파님 시간표

---
+++ src/app/(root)/time-table/7db115f4-b595-4dc3-9843-c151e9f089be/_img/imgs.ts
@@ -0,0 +1,42 @@
+import { ImgsType } from '@/types/time-table/image';
+
+import ArtistImg from './main/artist.png';
+import MainBG from './main/bg.png';
+import MainFrame from './main/frame.png';
+import TopObject from './main/top_object.png';
+import WeeklyMemo from './main/weekly_memo.png';
+
+import MultiA from './main/multi_a.png';
+import MultiB from './main/multi_b.png';
+import MultiC from './main/multi_c.png';
+import OfflineA from './main/offline_a.png';
+import OfflineB from './main/offline_b.png';
+import OfflineC from './main/offline_c.png';
+import OfflineMemoA from './main/offline_memo_a.png';
+import OfflineMemoB from './main/offline_memo_b.png';
+import OfflineMemoC from './main/offline_memo_c.png';
+import OnlineA from './main/online_a.png';
+import OnlineB from './main/online_b.png';
+import OnlineC from './main/online_c.png';
+
+export const Imgs: ImgsType = {
+  first: {
+    bg: MainBG,
+    artist: ArtistImg,
+    topObject: TopObject,
+    profileFrame: MainFrame,
+    weeklyMemo: WeeklyMemo,
+    a_online: OnlineA,
+    a_offline: OfflineA,
+    a_offline_memo: OfflineMemoA,
+    a_multi: MultiA,
+    b_online: OnlineB,
+    b_offline: OfflineB,
+    b_offline_memo: OfflineMemoB,
+    b_multi: MultiB,
+    c_online: OnlineC,
+    c_offline: OfflineC,
+    c_offline_memo: OfflineMemoC,
+    c_multi: MultiC,
+  },
+};
