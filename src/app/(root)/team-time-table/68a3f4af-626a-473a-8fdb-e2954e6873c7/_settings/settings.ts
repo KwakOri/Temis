668ab84b1e2201b6547acd5c11d73813de775aba
kwakjaehun <jh5414092@icloud.com>
2026-08-03 14:43:13 +0900

    Feat: 팀 시간표 수정

--- src/app/(root)/team-time-table/68a3f4af-626a-473a-8fdb-e2954e6873c7/_settings/settings.ts
+++ src/app/(root)/team-time-table/68a3f4af-626a-473a-8fdb-e2954e6873c7/_settings/settings.ts
@@ -162,7 +162,7 @@ export const Settings: SettingsType = {
   },
 };
 
-export const team_ids = [5, 359, 247];
+export const team_ids = [5, 39, 359, 247];
 
 export type TeamTimeTableMemberOrder = 'time' | 'member';
 
@@ -174,13 +174,15 @@ export const memberOrder = team_ids;
 export const dummyUnregisteredMemberIds: number[] = [];
 
 export const memberIdsMap = new Map([
-  [39, 'miruru'],
+  [5, 'miruru'],
+  [39, 'saac'],
   [359, 'pukong'],
   [247, 'liffy'],
 ]);
 
 export const memberNamesMap = new Map([
-  [39, '미루루'],
+  [5, '미루루'],
+  [39, '사악이'],
   [359, '푸콩이'],
   [247, '리피'],
 ]);
